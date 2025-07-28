27bb2adcfcc2d58c8a07ea090965d5bce55a4abb
Emmanuel Durotimi <90837486+Durotemy@users.noreply.github.com>
2025-07-28 16:36:00 +0100

    map feature

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,30 +1,17 @@
 import Link from "next/link";
-import {
-  IoHomeOutline,
-  IoPersonOutline,
-  IoNotificationsOutline,
-} from "react-icons/io5";
 
-const navItems = [
-  { href: "/", label: "Home", icon: <IoHomeOutline size={24} /> },
-  { href: "/about", label: "About", icon: <IoPersonOutline size={24} /> },
-  {
-    href: "/notifications",
-    label: "Alerts",
-    icon: <IoNotificationsOutline size={24} />,
-  },
-];
+import { navItems } from "@/contants";
 
 export default function BottomNavBar() {
   return (
-    <nav className="flex md:hidden fixed bottom-0 left-0 right-0 z-50 bg-okunGreen justify-around items-center h-16 shadow-t text-white">
+    <nav className="flex md:hidden fixed bottom-7 left-1/2 -translate-x-1/2 z-50 bg-okunGreen justify-around items-center h-16 shadow-t text-white mx-auto  w-11/12 rounded-full">
       {navItems.map((item) => (
         <Link
           key={item.href}
           href={item.href}
           className="flex flex-col items-center justify-center flex-1 py-2 hover:text-okunBlue transition-colors"
         >
-          {item.icon}
+          {<item.icon size={24} />}
           <span className="text-xs mt-1">{item.label}</span>
         </Link>
       ))}
--- src/contants.ts
+++ src/contants.ts
@@ -1,9 +1,49 @@
+import { IoHomeOutline } from "react-icons/io5";
+import { GoCommentDiscussion } from "react-icons/go";
+import { MdHistoryEdu } from "react-icons/md";
+import { FiMap } from "react-icons/fi";
+import { MdCastForEducation } from "react-icons/md";
+
 export const navBarPrimary = [
   { id: 1, title: "Home", link: "/" },
   { id: 2, title: "Discussion", link: "https://x.com/okunvoice" },
   { id: 3, title: "Map", link: "/map" },
   { id: 4, title: "Hotlines", link: "/hotlines" },
   { id: 5, title: "Profile", link: "/profile" },
+];
+
+export const navItems = [
+  {
+    id: 1,
+    href: "/",
+    label: "Home",
+    icon: IoHomeOutline,
+  },
+  {
+    id: 2,
+    href: "/education",
+    label: "Education",
+    icon: MdCastForEducation,
+  },
+  {
+    id: 3,
+    href: "/map",
+    label: "Map",
+    icon: FiMap,
+  },
+  {
+    id: 4,
+    href: "/discussion",
+    label: "Discussion",
+    icon: GoCommentDiscussion,
+  },
+
+  {
+    id: 5,
+    href: "/history",
+    label: "History",
+    icon: MdHistoryEdu,
+  },
 ];
 
 export const navBarSecondary = [
